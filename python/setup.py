a8f8e8166afef9f2279a7a1a104ee7469afebf2e
Davies Liu <davies.liu@gmail.com>
2010-05-14 08:26:17 +0000

    bugfix: package beansdb in setup.py, thanks to Zsp007

--- python/setup.py
+++ python/setup.py
@@ -2,10 +2,15 @@ from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [
+    name="beansdb",
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
         Extension("store", ["store.pyx", "../htree.c", "../hstore.c"],
                 libraries=["tokyocabinet"],
     )],
+    packages=["beansdb"],
     test_suite="test",
+    install_requires=['cython'],
+    zip_safe=False,
+    version="0.3.0",
 )
