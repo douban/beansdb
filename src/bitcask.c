18e33248132a35182781c03c9a2f91983adad2c1
yangxiufeng <yangxiufeng@douban.com>
2014-08-20 11:07:59 +0800

    fix null pointer

--- src/bitcask.c
+++ src/bitcask.c
@@ -1037,13 +1037,15 @@ DataRecord* bc_get(Bitcask *bc, const char* key)
         // check key
         if (strcmp(key, r->key) != 0)
         {
-
             if (bc->optimize_flag == 0)
                 log_error("Bug: record %s is not expected %s in %u @ %u", r->key, key, bucket, pos);
             free_record(r);
             r = NULL;
         }
-        r->version = item->ver; 
+        else
+        {
+            r->version = item->ver; 
+        }
     }
 GET_END:
     if (NULL == r && bc->optimize_flag == 0)
